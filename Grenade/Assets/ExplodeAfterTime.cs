4fd27869b54df572034e48aabfb0292fe90173c0
Filip Komarzyniec <filip.komarzyniec@gmail.com>
2020-04-14 14:08:28 +0200

    Grenade project added

---
+++ Grenade/Assets/ExplodeAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeAfterTime : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Explode", 5);
+    }
+
+    void Explode() {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);  // z małej litery w C# są zmienne 
+    }
+}
---
+++ Grenade/Assets/GrenadeThrower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour
+{
+    public Rigidbody grenadePrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) {
+           var grenade =  Instantiate(grenadePrefab, transform.position, Quaternion.identity);
+           grenade.AddForce(transform.forward*300);
+        }
+    }
+}
