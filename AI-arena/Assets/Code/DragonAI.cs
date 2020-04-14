3f048ff144d357aa87d9043935d9beae6c7d0321
Filip Komarzyniec <filip.komarzyniec@gmail.com>
2020-04-14 14:06:56 +0200

    AI project added

---
+++ AI-arena/Assets/Code/DragonAI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class DragonAI : MonoBehaviour {
+    GameObject player;
+    DragonAnimator dragonAnimator;
+	Rigidbody rigidbody;
+	
+	public enum State {
+		Running, Dead
+	}
+	State state = State.Running;
+
+	void Start () {
+		dragonAnimator = GetComponent<DragonAnimator>();
+		rigidbody = GetComponent<Rigidbody>();
+	    player = GameObject.FindGameObjectWithTag("Player");
+	    
+
+	}
+	void Update () {		
+		switch (state) {
+			case State.Running:
+				RunToPlayer();
+				AttackPlayerifCloseEnough();
+				break;
+			case State.Dead:
+				break;
+		}	
+	}
+	void RunToPlayer() {
+		dragonAnimator.PlayRunAnimation();
+		transform.LookAt(player.transform);
+		rigidbody.velocity = transform.forward * 5;
+	}
+	void AttackPlayerifCloseEnough() {
+		if (Vector3.Distance(transform.position, player.transform.position) < 10) {
+	        dragonAnimator.PlayAttackAnimation();
+            EndGameUi.ShowTextAndQuit("YOU DIED");
+        }
+	}
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Trap") {
+			dragonAnimator.PlayDeadAnimation();
+			state = State.Dead;
+		}
+	}
+}
